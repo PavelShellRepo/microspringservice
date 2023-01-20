d0199fccaa5e72ee04c7608c65572834e6d9f3d8
PavelShellXPro <pavelshell2022@gmail.com>
2023-01-20 07:40:20 +0200

    Initial commit

---
+++ src/main/java/com/demoproject/microspringservice/MicrospringserviceApplication.java
@@ -0,0 +1,13 @@
+package com.demoproject.microspringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrospringserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrospringserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demoproject/microspringservice/MicrospringserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoproject.microspringservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrospringserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
